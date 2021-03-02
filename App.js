ce028d93ee0bbf301890c4a5826c00c0d0e9ebe1
vio9j6l <jpl5840@psu.edu>
2021-03-02 10:29:22 -0500

    edit app.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   state = {
     gameId: 1,
   };
-  
+
   // resetGame function
   resetGame = () => {
     this.setState((prevState) => {
@@ -24,6 +24,10 @@ class App extends React.Component {
     );
   }
 };
+
+
+
+
 
 
 export default App;
